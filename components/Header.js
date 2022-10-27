851fb983caa07e8a4b28375a13523be9f5276d9c
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-10-27 11:59:44 +0200

    first-commit

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import { PrismicLink, PrismicText } from "@prismicio/react";
+import * as prismicH from "@prismicio/helpers";
+
+export const Header = ({ navigation, settings }) => {
+  return (
+    <div className="header">
+
+    </div>
+  );
+};
